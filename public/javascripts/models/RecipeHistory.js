7fc6fd9c15cd616c8129560aba7be2ec1ea378ad
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2011-05-20 22:30:43 +0200

    fix in history and Recipehistory

--- public/javascripts/models/RecipeHistory.js
+++ public/javascripts/models/RecipeHistory.js
@@ -1,7 +1,7 @@
 var RecipeHistory = Backbone.Collection.extend({
 	
-	initialize = function(recipe_id){
-		this.recipe_id = recipe.id;
+	initialize : function(recipe_id){
+		this.recipe_id = recipe_id;
 	},
 	
 	recipe_id: null,
