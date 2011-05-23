26e75bdeb7a0b6ebf9e0df396e70186e66c0e4ff
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2011-05-23 23:38:29 +0200

    Every day i'm shuffling

--- public/javascripts/views/recipe.js
+++ public/javascripts/views/recipe.js
@@ -11,17 +11,37 @@ MK.Views.Recipe = Backbone.View.extend({
 		this.template = new EJS({url : '/javascripts/views/recipe.ejs'}); 
 	//	passer {model : recipeinstance} en argument
 		this.id = "recipe"+options.model.id;
+		//_.bindAll()
+		//$(this.el).delegate(".topp","click" , this.addWith);
+		//$(this.el).delegate(".bott","click" , this.addWithout);
 	//TODO binder les changements sur les models
 	
 	},
+	
+	addWith : function(event){
+		index = $(event.target).data("ingredient");
+		
+	//	var index = $(this).data("ingredient");
+		try{console.log(this.model.attributes.ingredients[index]);}
+		catch(e){
+			console.log(e);
+		}
+		return false;
+	},
+	
+	addWithout : function(){
+	//	console.log(this.data("ingredient"));
+		return false;
+	},
+	
 	// 
 	events : {
 		"click .like_pic"		: "clickLike",
 		"click .check_pic"		: "clickDone",
 		"click .star_pic"		: "clickFavorite",
-		"click .foll_pic"		: "clickFollow"
+		"click .foll_pic"		: "clickFollow",
 		"click .topp"			: "addWith",
-		"click .bott"			: "addWitout"
+	//	"click .bott"			: "addWitout"
 	// 	// 	//event edit handle by route controler
 	},
 	
