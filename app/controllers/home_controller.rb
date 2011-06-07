2b869d2a454c10241a3e0d6067f346501181994b
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2011-06-08 01:02:58 +0200

    fix recipe json format

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,14 +13,5 @@ class HomeController < ApplicationController
       render :index
     end
   end
-  
-  # def dashboard
-  #     if !user_signed_in?
-  #       redirect_to root_path
-  #     else
-  #       @user = current_user
-  #       respond_with @user
-  #     end
-  #   end
     
 end
