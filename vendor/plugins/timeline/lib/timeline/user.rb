7cafa096a379fef1fc05a7e73c2fefa7e69b848a
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2011-06-07 18:37:57 +0200

    fix destroy timeline after destroy user

--- vendor/plugins/timeline/lib/timeline/user.rb
+++ vendor/plugins/timeline/lib/timeline/user.rb
@@ -1,8 +1,6 @@
 module Timeline
   module User
 
-    after_destroy :delete_user_timelines
-
     def timelined?
       false
     end
@@ -28,7 +26,9 @@ module Timeline
         instance_variable_get(variable) || instance_variable_set(variable, Timeline::Feed.new(feed_name, self, followers))
       end
       
-      define_method :delete_user_timelines do
+      after_destroy "delete_user_timeline_#{feed_name}"
+      private
+      define_method "delete_user_timeline_#{feed_name}" do
         self.all_feeds.each do |feed_name|
           send(feed_name).destroy
         end
